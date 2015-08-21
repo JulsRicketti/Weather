70fdb7b71812a04753f905c845697c16f75843c6
Julia Ricketti <julsricketti@gmail.com>
2015-08-21 17:11:34 -0400

    getLocation() function organized

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-$.get("http://ipinfo.io", function(response) {
-    $("#ip").html(response.ip);
-    $("#address").html(response.city + ", " + response.region);
-}, "jsonp");
+$(document).ready(function(){
+	
+	getLocation();
+	function getLocation(){
+		$.get("http://ipinfo.io", function(response) {
+	    $("#ip").html(response.ip);
+	    $("#address").html(response.city + ", " + response.region);
+	}, "jsonp");
+	}
+});
+
