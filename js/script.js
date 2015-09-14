a960f0bfaad01aae7bc8efaa890f592def5445dc
Julia Ricketti <julsricketti@gmail.com>
2015-09-14 15:37:20 -0400

    Weather system now working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,74 @@
 $(document).ready(function(){
 	
+	var city;
+	
+	
 	getLocation();
 	function getLocation(){
 		$.get("http://ipinfo.io", function(response) {
-	    $("#ip").html(response.ip);
-	    $("#address").html(response.city + ", " + response.region);
-	}, "jsonp");
+		    $("#address").html(response.city + ", " + response.region);
+		    city = response.city;
+		    var units = getUnits(response.country);
+		    getWeather(response.loc, units);
+		    
+		}, "jsonp");
+	
 	}
+	
+	function getWeather(loc, units) {
+	    lat = loc.split(",")[0];
+	    lon = loc.split(",")[1];
+	
+	    var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + "&units=" + units;
+	
+	    console.log(weatherApiUrl);
+	
+	    $.get(weatherApiUrl, function(weather) {
+	      var windDir = convertWindDirection(weather.wind.deg);
+	      var temperature = weather.main.temp;
+	      var unitLabel;
+	
+	      //label based in imperial vs metric units
+	      if (units === "imperial") {
+	        unitLabel = "F";
+	      } else {
+	        unitLabel = "C";
+	      }
+	
+	      temperature = parseFloat((temperature).toFixed(1));
+	
+	      console.log(weather);
+	
+	      $('#icon').append("<img src='http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png'>");
+	
+	      $('#temp').append(temperature + " " + unitLabel);
+	      $('#conditions').append(weather.weather[0].description);
+	      $('#wind').append(windDir + " " + weather.wind.speed + " knots");
+	      $('#postal').append(postal);
+
+    }, "jsonp");
+
+  };
+  
+   function convertWindDirection(dir) {
+    var rose = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    var eightPoint = Math.floor(dir / 45);
+    return rose[eightPoint];
+  }
+
+  function getUnits(country) {
+    var imperialCountries = ['US', 'BS', 'BZ', 'KY', 'PW'];
+
+    if (imperialCountries.indexOf(country) === -1) {
+      var units = 'metric';
+    } else {
+      units = 'imperial';
+    }
+
+    console.log(country, units);
+    return units;
+  }
+
+
 });
 
